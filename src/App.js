29c8eb538a1bfc0729df3c31c78f4c73bbf42227
Ranko Radonić <ranko.radonic@gmail.com>
2018-02-15 13:11:34 -0500

    Add JS and React basics

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    console.log('wfp');
+
+    return (
+      <div className="App">
+        Hello World!
+      </div>
+    );
+  }
+}
+
+export default App;
