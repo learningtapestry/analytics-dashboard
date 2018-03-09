9591ef025c4135c79f21d22bd872f3623b5e322b
Ranko Radonić <ranko.radonic@gmail.com>
2018-03-09 15:48:55 -0500

    Just serve the React app blob directly

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/lti_launch', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.get('/static/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', req.originalUrl));
-});
-
-app.get('/', function (req, res) {
-  res.send('Root path');
-});
+app.use(express.static('build'));
 
 app.listen(process.env.PORT || 8080);
